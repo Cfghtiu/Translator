57af7b4e164ce92f0da6f8c0a8cd2c18c4f70d57
KGG_XK <1838926478@qq.com>
2025-06-16 18:54:03 +0800

    准备添加shift+enter翻译

--- src/main/java/kgg/translator/mixin/hud/ChatScreenMixin.java
+++ src/main/java/kgg/translator/mixin/hud/ChatScreenMixin.java
@@ -1,16 +1,32 @@
 package kgg.translator.mixin.hud;
 
+import kgg.translator.Translate;
+import kgg.translator.exception.TranslateException;
 import kgg.translator.handler.ChatHandler;
+import kgg.translator.translator.Source;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
+    @Shadow protected TextFieldWidget chatField;
+
+    @Shadow public abstract void sendMessage(String chatText, boolean addToHistory);
+
     @Inject(method = "init", at = @At(value = "HEAD"))
     public void init(CallbackInfo ci) {
         ChatHandler.addTip();
@@ -20,6 +36,27 @@ public abstract class ChatScreenMixin extends Screen {
     protected ChatScreenMixin(Text title) {
         super(title);
     }
+
+    @Inject(method = "keyPressed", at = @At(value = "HEAD"), cancellable = true)
+    public void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+//        if (keyCode == GLFW.GLFW_KEY_ENTER || keyCode == GLFW.GLFW_KEY_KP_ENTER && hasShiftDown()) {
+//            // 翻译选中内容
+//            String text = chatField.getText();
+//            client.setScreen(null);
+//            client.player.sendMessage(Text.literal("[翻译中]"), false);
+//            CompletableFuture.runAsync(() -> {
+//                try {
+//                    String result = Translate.cachedTranslate(text, Source.CHAT);
+//                    Text msg = Text.literal(result).setStyle(Style.EMPTY.withInsertion(text).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, text)).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击复制"))));
+//                    client.player.sendMessage(msg, false);
+//                } catch (TranslateException e) {
+//                    client.player.sendMessage(Text.literal("[失败]"), false);
+//                }
+//            });
+//            cir.setReturnValue(true);
+//        }
+    }
+
 
     @Override
     public void removed() {
