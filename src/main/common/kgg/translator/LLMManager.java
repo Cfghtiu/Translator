7985985bb64742423688d45f22508cfbc7e65408
wrds <2598142880@qq.com>
2025-07-18 15:14:07 +0800

    readme增加todo

--- src/main/common/kgg/translator/LLMManager.java
+++ src/main/common/kgg/translator/LLMManager.java
@@ -89,6 +89,7 @@ public class LLMManager {
             this.apiKey = apiKey == null ? "" : apiKey;
         }
     }
+
     public static void setModels(Map<String, Model> newModels) {
         models.clear();
         models.putAll(newModels);
